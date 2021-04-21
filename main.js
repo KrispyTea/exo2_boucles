a68258230a29fe6f6462ae81921a97aecca73a57
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-04-21 15:10:18 +0200

    over

---
+++ main.js
@@ -0,0 +1,13 @@
+//exo1
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+eleves.forEach(element => {
+    console.log("Bonjour " + element);
+});
+
+//exo2
+
+let array = ["pomme", "poire", "cadeau", "retho", "intra"]
+array.forEach(element => {
+    console.log(element)
+})
